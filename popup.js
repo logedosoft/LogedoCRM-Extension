884b03bb05a9fc0dddcd49d6c6bb5e32960ca2cb
barancaliskan <barancaliskan86@gmail.com>
2024-05-07 15:45:48 +0300

    kullanıcı inputu ile veriyi local storage'e kaydetme eklendi.

--- popup.js
+++ popup.js
@@ -1,6 +1,19 @@
-chrome.tabs.query({'active': true, currentWindow:true}, 
-function(tabs){
-	var tab = tabs[0];
-    var h1Tag = document.getElementsByTagName("h1");
-    h1Tag[0].innerText = tab.url;
+var saveButton = document.getElementById("save_data");
+var serverName = document.getElementById("server_name");
+
+saveButton.addEventListener("click", function(){
+    if(serverName.value){
+        chrome.storage.sync.set({ "server_name": serverName.value }, function(){
+            console.log("server_name => " + serverName.value + " set edildi.");
+            var h1Tag = document.getElementsByTagName("h1");
+            h1Tag[0].innerText = serverName.value;
+        });
+    }
+});
+
+chrome.storage.sync.get(["server_name"]).then((result) => {
+    if(result.server_name){
+        var h1Tag = document.getElementsByTagName("h1");
+        h1Tag[0].innerText = result.server_name;
+    }
 });
