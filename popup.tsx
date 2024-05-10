2f454c4cee88ab2fd6006b4b160b7decfe700349
barancaliskan <barancaliskan86@gmail.com>
2024-05-10 18:13:31 +0300

    Eklenti bir önceki versiyon da plasmo'yu kullanmıyormuş. Artık plasmo framework'ü kullanıyor hata düzeltildi.

--- popup.tsx
+++ popup.tsx
@@ -1,13 +1,38 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [currentUrl, setCurrentUrl] = useState<string>();
+
+  const getCurrentUrl = async (): Promise<void> => {
+    let tab = chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true,
+      },
+      function (tabs) {
+        const tab = tabs[0];
+        if (tab.url) {
+          debugger;
+          setCurrentUrl(tab.url);
+        }
+      },
+    )
+  };
+
+  useEffect(() => {
+    getCurrentUrl();
+  }, []);
 
   return (
-    {
-
-    }
-  )
+    <div
+      style={{
+        padding: 16,
+      }}
+    >
+      <input type="text" />
+      <h2>This site is {currentUrl}</h2>
+    </div>
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
